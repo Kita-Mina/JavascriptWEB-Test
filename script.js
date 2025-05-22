4f368e62be7abbf7b1a26ff63a3584c9db5c97f9
Kita-Mina <167517176+Kita-Mina@users.noreply.github.com>
2025-05-22 15:41:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+const images = [
+  "image1.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg"
+]
+
+let index = 0
